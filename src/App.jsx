66031db551816055371e89decbf96c3f46db5849
Maddi <maddi.fitzmaurice@gmail.com>
2025-08-27 13:29:03 +1000

    HTML skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Pomo Timer</h1>
+      <div className="status-bar"></div>
+      <div className="timer-box">
+        <div className="timer-type-box">
+          <button>Pomodoro</button>
+          <button>Short Break</button>
+          <button>Long Break</button>
+        </div>
+        <h2>25:00</h2>
+        <div className="timer-func-box">
+          <button>Reset</button>
+          <button>Pause/Play</button>
+          <button>Skip</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="info-box">
+        <p>#1</p>
+        <p>Time to focus!</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
